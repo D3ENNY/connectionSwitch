d2ffb0b2ec9f3aab17a3914679f55d9a15d6c1cf
Denys Raimondi <denys.raimondi@sinetinformatica.it>
2024-04-10 16:45:36 +0200

    initialize script

---
+++ connectionManager.ps1
@@ -0,0 +1,28 @@
+function connect() { param([Parameter(Mandatory=$true)] [Object]$Json)
+    Write-Host $Json
+}
+
+$connections = (Get-Content .\assets\config.json -Raw | ConvertFrom-Json).connections
+
+Write-Output "A che rete LAN vuoi connetterti?"
+
+foreach ($i in $connections) {
+    foreach ($key in $i.PSObject.Properties.Name) {
+        $cnt = $connections.IndexOf($i) + 1
+        Write-Output "$cnt : $key"
+    }
+}
+
+$choise = (Read-Host "selezionare il numero scelto") - 1
+
+try {
+    if ($choice -ge 0 -and $choice -lt $connections.Length){
+        connect -JSON $connections.GetValue($choise)
+    }else{
+        Write-Host "Si e' verificato un errore durante l'accesso all'elemento, l'elemento $choise non esiste"
+    }
+}
+catch {
+    Write-Host "Si e' verificato un errore durante l'elaborazione dell'oggetto JSON: " 
+    Write-Error $_
+}
