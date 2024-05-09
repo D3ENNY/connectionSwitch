1258c3c4591ccbbeea7e15f587598731eac45970
D3ENNY <denysraimondi06@gmail.com>
2024-05-09 08:58:22 +0200

    add check for admin privileges

--- connectionManager.ps1
+++ connectionManager.ps1
@@ -54,6 +54,12 @@ function Configure-DHCPSettings {
     $interface | Set-DnsClientServerAddress -ResetServerAddresses
 }
 
+# Admin privileges check
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
 Clear-Host
 # Read static JSON file
 $config = Get-Content .\assets\config.json -Raw | ConvertFrom-Json
